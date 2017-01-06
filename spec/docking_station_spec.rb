6cc7dfa3c70a912debec9c452a7196e3cfbfa30e
Matthew Perry <thesedatedprince@gmail.com>
2017-01-06 15:22:57 +0000

    Finished excersize 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,16 +3,19 @@ require './lib/docking_station'
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
 
-
+  #let(:bike) {double :bike}
+  let(:bike) {double(:bike, :working?  => true)}
   it 'should respond to working method' do
-    bike = double(:bike)
+    #bike = double(:bike)
+    #allow(bike).to receive(:working?).and_return(true)
     expect(bike).to respond_to :working?
   end
 
   it {is_expected.to respond_to :dock}
   it { is_expected.to respond_to(:dock).with(1).argument}
   it 'should be able to dock bike'do
-    bike = double(:bike)
+    #bike = double(:bike)
+    bike = bike
     expect(subject.dock(bike)).to eq [bike]
   end
 
@@ -23,13 +26,13 @@ end
 
 
 it 'should raise an error if station is full' do
-  subject.capacity.times{subject.dock double(:bike)}
-  expect {subject.dock double(:bike)}.to raise_error('station is full')
+  subject.capacity.times{subject.dock bike}
+  expect {subject.dock bike}.to raise_error('station is full')
 end
 
 it {is_expected.to respond_to(:report_broken)}
 it 'should be able to report broken bikes'do
-    bike = double(:bike)
+    bike = bike
     subject.dock(bike)
     expect(subject.report_broken).to eq [bike]
 end
